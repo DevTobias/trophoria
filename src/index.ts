82755e3bb5ff5e9b5cd28c1e85036dcf50a3690a
Tobias Kärst <t.kaerst@bora.com>
2023-08-09 10:46:22 +0200

    feat: optimized docker file

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Elysia } from "elysia";
 
 const app = new Elysia()
   .get("/", () => "Hello Elysia")
-  .listen(3000);
+  .listen(8080);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
